f22922819d93d134c1d3c4e3a26b2e4df7d9619d
grinyyaaa <144795933+grinyyaaa@users.noreply.github.com>
2023-11-21 13:34:50 +0200

    123

--- lab_3_task_3.py
+++ lab_3_task_3.py
@@ -1,20 +1,25 @@
 import numpy as np
-
+from lab_3_task_1 import mg_acceleration as maa
 
 x0 = 0 
 v0 = 10
 v0x = np.cos(30) * v0
 
-from lab_3_task_1 import mg_acceleration as maa
- 
 y0 = 0
 v0y = np.sin(30) * v0
 
-t = np.arange(0, 5, 0.01)
-
+t = np.arange(0, 5, 0.1)
+print(t)
 x = x0 + v0x * t 
 y = y0 + v0y * t - maa * t ** 2 / 2
 
-print(x)
-print(y)
+i = np.arange(len(x))
+
+mass = np.zeros((len(x), 3))
+
+mass[i,0] = t[i]
+mass[i,1] = x[i]
+mass[i,2] = y[i]
+print(mass)
+
 
