2c893e707cbe14750d2490f2935178479443d317
grinyyaaa <144795933+grinyyaaa@users.noreply.github.com>
2023-11-28 14:27:47 +0200

    123123123123123123123123123123

---
+++ lec_4_new_data.py
@@ -0,0 +1,25 @@
+x = 3 
+y = 4 
+
+z = complex(x, y)
+print(z)
+
+w = complex(y, x)
+print(z + w)
+
+a = 'hello'
+print(a[0])
+
+# a[0] = 'H' 
+t = (1, 4, 9)
+print(t)
+print(t[0])
+# t[0] = 23
+
+d = {'al':4, 4:'al', 'str':'hello'}
+print(d['al'])
+print(d[4])
+print(d['str'])
+
+d['str'] = 'good'
+print(d)
