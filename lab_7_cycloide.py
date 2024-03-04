cd578ff99ce6595ad70cdd58d13c53294fb67048
grinyyaaa <144795933+grinyyaaa@users.noreply.github.com>
2024-03-04 23:21:53 +0200

    423423423423

--- lab_7_cycloide.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-import matplotlib.pyplot as plt
-a = 1
-for t in np.arange(1, 8*np.pi, 0.01):
-    x = a(t - np.sin(t))
-    y = a(t - np.cos(t))
-    plt.plot(x, y)
-plt.savefig('fig_1.png')
