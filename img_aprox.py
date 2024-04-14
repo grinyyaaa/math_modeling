54b5a91070be5df46cde3dc0442cd7e64dfd038a
grinyyaaa <144795933+grinyyaaa@users.noreply.github.com>
2024-04-14 23:13:06 +0200

    231231sdad1213123dasdasdas

--- img_aprox.py
+++ img_aprox.py
@@ -7,7 +7,25 @@ fig, ax = plt.subplots()
 
 ax.imshow(img)
 
+t = np.linspace(np.pi/2, np.pi/4, 100)
+x = 1000 + 100*np.cos(t)
+y = 620 + 100*np.sin(t)
+
+ax.plot(x, y, '-', lw=2, color='b')
+
+x = [1075, 1115]
+y = [685, 685]
+
+ax.plot(x, y, '-', lw=2, color='b')
+
+t = np.linspace(2.3*np.pi/2, 2.7* np.pi/2, 100)
+x = 1205 + 100*np.cos(t)
+y = 730 + 100*np.sin(t)
+
+ax.plot(x, y, '-', lw=2, color='b')
 
 plt.ylim(1000, 0)
-plt.xlim(1000, 1500)
+plt.xlim(850, 1500)
 plt.savefig('star.png')
+
+
--- lec_1_aprox.py
+++ lec_1_aprox.py
@@ -24,6 +24,42 @@ y = 7 + 5*np.sin(t)
 
 ax.plot(x, y, '-', lw=2, color='k')
 
+
+t = np.linspace(np.pi/2+np.pi/4, np.pi/2-np.pi/4, 100)
+x = 2.75 + np.cos(t)
+y = 4.83 + np.sin(t)
+
+ax.plot(x, y, '-', lw=2, color='k')
+
+t = np.linspace(3*np.pi/2-np.pi/4, 3*np.pi/2+np.pi/4, 100)
+x = 2.75 + np.cos(t)
+y = 6.21 + np.sin(t)
+
+ax.plot(x, y, '-', lw=2, color='k')
+
+t = np.linspace(3*np.pi/2-np.pi/4, 3*np.pi/2+np.pi/4, 100)
+x = 5.3 + np.cos(t)
+y = 6.21 + np.sin(t)
+
+ax.plot(x, y, '-', lw=2, color='k')
+
+t = np.linspace(np.pi/2+np.pi/4, np.pi/2-np.pi/4, 100)
+x = 5.3 + np.cos(t)
+y = 4.83 + np.sin(t)
+
+ax.plot(x, y, '-', lw=2, color='k')
+
+x = [4, 3.5, 4.5, 4]
+y = [3.8, 4.48, 4.48, 3.8]
+
+ax.plot(x, y, '-', lw=2, color='k')
+
+t = np.linspace(3*np.pi/2-np.pi/4, 3*np.pi/2+np.pi/4, 100)
+x = 4 + np.cos(t)
+y = 3.85 + np.sin(t)
+
+ax.plot(x, y, '-', lw=2, color='k')
+
 plt.axis('equal')
 
 plt.savefig('fox.png')
